2dca6c23ab534967142e6959dc66c86780da6a4d
kalyan936 <157389210+kalyan936@users.noreply.github.com>
2026-05-23 16:22:31 +0530

    Update main.tsx

--- giftzee---classic-gift-shop-&-personal-box-builder/src/main.tsx
+++ giftzee---classic-gift-shop-&-personal-box-builder/src/main.tsx
@@ -1,15 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-function App() {
-  return (
-    // 👇 ADD BASENAME HERE
-    <BrowserRouter basename="/Giftzee/"> 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* your other routes */}
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
